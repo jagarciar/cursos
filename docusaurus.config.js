950eb0926589634c9bcf885824b9b2974d4bffb7
jeysongarcia <jeyson.garcia@neoris.com>
2025-04-24 13:33:52 -0500

    Modificación

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://github.com/jagarciar',
+  url: 'https://jagarciar.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/cursos/',
+  baseUrl: '/cursos',
   trailingSlash:false,
 
   // GitHub pages deployment config.
