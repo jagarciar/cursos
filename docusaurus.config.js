2b26677cc3bf2f01a1e540515d83c9721a8ec6b3
jeysongarcia <jeyson.garcia@neoris.com>
2025-05-18 18:25:49 -0500

    Modificación del baseURL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,7 +27,7 @@ const config = {
   organizationName: 'jagarciar', // Usually your GitHub org/user name.
   projectName: 'cursos', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
