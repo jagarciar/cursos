fe71a95ffcd92aea66a49dc455f52817763acce9
jagarciar <jeogarod@gmail.com>
2025-05-19 14:32:57 +0000

    deploy: dcd28f988b1508ac8e90d821e86f851db08d2c1a

---
+++ assets/js/2dc24c27.4e155764.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcursos=self.webpackChunkcursos||[]).push([[2240],{3964:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/cursos/blog","page":1,"postsPerPage":4,"totalPages":1,"totalCount":4,"blogDescription":"Un blog existe cuando la teoria, los conceptos y los lineamientos tienen variantes o se deben extender.","blogTitle":"Blog"}}')}}]);
